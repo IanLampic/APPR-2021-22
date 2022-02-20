8d465f7f1c01ade34f1ed14b6320a7cadfc06886
Ian Lampic <lampic.ian@gmail.com>
2022-02-20 11:18:54 +0100

    Uvoz

--- lib/libraries.r
+++ lib/libraries.r
@@ -10,3 +10,4 @@ options(gsubfn.engine="R")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+
--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,4 +1,9 @@
 # 2. faza: Uvoz podatkov
-
-
 sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+
+Vzroki <- read_csv("podatki/Vzroki/podatki1.csv", na=",", locale=locale(encoding="Windows-1250"))
+Zdrava <- read_csv("podatki/Zdravaleta/podatki2.csv", na=",", locale=locale(encoding="Windows-1250"))
+Potrosnja <- read_csv("podatki/Potrošnja/podatki3.csv", na=",", locale=locale(encoding="Windows-1250"))
+Prebivalstvo <- read_tsv("podatki/število_prabivalstva.tsv", na=" ", locale=locale(encoding="Windows-1250"))
+
+#Ugotovit, katere države so pri prebivalstvu, končnice
